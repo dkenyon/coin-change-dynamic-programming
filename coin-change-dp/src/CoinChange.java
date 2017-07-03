2b17e03789c8a1758dddfc2fc12d258298d0c375
Dennis Kenyon <dkenyon@uw.edu>
2017-07-03 12:33:51 -0700

    Fixed console output formatting

--- coin-change-dp/src/CoinChange.java
+++ coin-change-dp/src/CoinChange.java
@@ -1,12 +1,20 @@
 import java.util.Arrays;
 
-public class CoinChange{
-
-	// the  value to find different combinations/min number of coins for
-	private static int VALUE = 4;
+/**
+ * Solves for and prints to the console two variants of the class coin change dynamic programming problem.
+ * Also prints 2D cache array to help check work.
+ * Note: As of Jul 2017, min coin method is bugged if there is no coin value for 1.
+ * Edit the variables VALUE and COIN_VALUES
+ * @author dkenyon
+ *
+ */
+public class CoinChange {
+	
+	// the value to find different combinations/min number of coins for
+	private static int VALUE = 12;
 
 	// denomination set of coin values
-	private static int[] COIN_VALUES = {1, 2, 3 };
+	private static int[] COIN_VALUES = { 1, 2, 5 };
 
 	// temporary storage to keep track of running totals
 	private static int[][] CACHE = new int[COIN_VALUES.length][VALUE + 1];;
@@ -14,18 +22,10 @@ public class CoinChange{
 	// main
 	public static void main(String[] theArgs) {
 		// set of coin values in ascending order
-		int solution = Integer.MIN_VALUE;
 		Arrays.sort(COIN_VALUES);
 
-		// solve here
-		solution = solveMaxCombinations();
-//		solution = solveMinCoins();
-		// solve here
-		
-		printCacheToConsole();
-		System.out.println("\nSOLUTION: " + solution);
-		
-
+		solveMaxCombinations();
+		solveMinCoins();
 	}
 
 	// solves the task of finding the different number of
@@ -57,14 +57,16 @@ public class CoinChange{
 				}
 			}
 		}
-
+		System.out.println("Max number of combinations to make " + VALUE + " with coin values " + Arrays.toString(COIN_VALUES) + ": " + solution + "\n");
+		printCacheToConsole();	
+		System.out.println("\n\n");
 		return solution;
 	}
 
 	// solves the task of finding the minimum number of
 	// coins to get the VALUE coin value.
 	// populates the cache and returns the solution.
-	// BUG 2017-07-03: does not work right if there is no 1-value coin 
+	// BUG 2017-07-03: does not work right if there is no 1-value coin
 	private static int solveMinCoins() {
 		int solution = Integer.MAX_VALUE;
 		int currentCoin = COIN_VALUES[0];
@@ -87,12 +89,14 @@ public class CoinChange{
 					CACHE[row][col] = CACHE[row - 1][col];
 				} else {
 					CACHE[row][col] = Math.min(CACHE[row - 1][col], CACHE[row][col - currentCoin] + 1);
-					
 				}
 			}
 		}
-		
-		solution = CACHE[CACHE.length - 1][CACHE[0].length -1 ];
+
+		solution = CACHE[CACHE.length - 1][CACHE[0].length - 1];
+		System.out.println("Min number of coins to make " + VALUE + " with coin values " + Arrays.toString(COIN_VALUES) + ": " +solution + "\n");
+		printCacheToConsole();
+		System.out.println("\n\n");
 		return solution;
 	}
 
@@ -121,7 +125,7 @@ public class CoinChange{
 	}
 
 	// creates a string of hyphens ('-') based off the VALUE's size
-	private static String printBorder() {
+	private static String printBorder() { 
 		StringBuilder string = new StringBuilder();
 
 		string.append("   +");
