927715fb70f9cc86e042a16e10adcbb2e605ceaa
Dennis Kenyon <dkenyon@uw.edu>
2017-07-04 09:41:22 -0700

    Fixed cache population issue (where set value > target)

--- coin-change-dp/src/CoinChange.java
+++ coin-change-dp/src/CoinChange.java
@@ -1,23 +1,24 @@
 import java.util.Arrays;
 
 /**
- * Solves for and prints to the console two variants of the class coin change dynamic programming problem.
- * Also prints 2D cache array to help check work.
- * Note: As of Jul 2017, min coin method is bugged if there is no coin value for 1.
- * Edit the variables VALUE and COIN_VALUES
+ * Solves for and prints to the console two variants of the class coin change
+ * dynamic programming problem. Also prints 2D cache array to help check work.
+ * Note: As of Jul 2017, min coin method is bugged if there is no coin value for
+ * 1. Edit the variables VALUE and COIN_VALUES
+ * 
  * @author dkenyon
  *
  */
 public class CoinChange {
-	
+
 	// the value to find different combinations/min number of coins for
-	private static int VALUE = 12;
+	private static int VALUE = 1;
 
 	// denomination set of coin values
-	private static int[] COIN_VALUES = { 1, 2, 5 };
+	private static long[] COIN_VALUES = {48, 6, 34, 50, 49, 36, 30, 35, 40, 41, 17, 43, 39, 13, 4, 20, 19, 2, 46, 7, 38, 33, 28, 18, 21 };
 
 	// temporary storage to keep track of running totals
-	private static int[][] CACHE = new int[COIN_VALUES.length][VALUE + 1];;
+	private static long[][] CACHE = new long[COIN_VALUES.length][VALUE + 1];;
 
 	// main
 	public static void main(String[] theArgs) {
@@ -25,15 +26,19 @@ public class CoinChange {
 		Arrays.sort(COIN_VALUES);
 
 		solveMaxCombinations();
-		solveMinCoins();
+		// solveMinCoins();
 	}
 
 	// solves the task of finding the different number of
 	// coin combinations to get the VALUE coin value.
 	// populates the cache and returns the solution.
-	private static int solveMaxCombinations() {
-		int solution = 0;
-		int currentCoin = COIN_VALUES[0];
+	private static long solveMaxCombinations() {
+		if (VALUE < COIN_VALUES[0]) {
+			System.out.println("0 possible combinations.");
+			return 0;
+		}
+		long solution = 0;
+		long currentCoin = COIN_VALUES[0];
 
 		// initialize first row
 		for (int col = 0; col < CACHE[0].length; col++) {
@@ -47,18 +52,21 @@ public class CoinChange {
 		for (int row = 1; row < CACHE.length; row++) {
 			currentCoin = COIN_VALUES[row];
 			for (int col = 0; col < CACHE[0].length; col++) {
-				if (currentCoin > col) {
+				if (currentCoin > VALUE) {
+					CACHE[row][col] = 0;
+				} else if (currentCoin > col) {
 					CACHE[row][col] = CACHE[row - 1][col];
 				} else {
-					CACHE[row][col] = CACHE[row - 1][col] + CACHE[row][col - currentCoin];
-					if (CACHE[row][col] > solution) {
+					CACHE[row][col] = CACHE[row - 1][col] + CACHE[row][col - (int) currentCoin];
+					if (CACHE[row][col] != 0) {
 						solution = CACHE[row][col];
 					}
 				}
 			}
 		}
-		System.out.println("Max number of combinations to make " + VALUE + " with coin values " + Arrays.toString(COIN_VALUES) + ": " + solution + "\n");
-		printCacheToConsole();	
+		System.out.println("Max number of combinations to make " + VALUE + " with coin values "
+				+ Arrays.toString(COIN_VALUES) + ": " + solution + "\n");
+		printCacheToConsole();
 		System.out.println("\n\n");
 		return solution;
 	}
@@ -67,9 +75,9 @@ public class CoinChange {
 	// coins to get the VALUE coin value.
 	// populates the cache and returns the solution.
 	// BUG 2017-07-03: does not work right if there is no 1-value coin
-	private static int solveMinCoins() {
-		int solution = Integer.MAX_VALUE;
-		int currentCoin = COIN_VALUES[0];
+	private static long solveMinCoins() {
+		long solution = Integer.MAX_VALUE;
+		long currentCoin = COIN_VALUES[0];
 
 		// initialize first row
 		for (int col = 0; col < CACHE[0].length; col++) {
@@ -88,13 +96,14 @@ public class CoinChange {
 				if (currentCoin > col) {
 					CACHE[row][col] = CACHE[row - 1][col];
 				} else {
-					CACHE[row][col] = Math.min(CACHE[row - 1][col], CACHE[row][col - currentCoin] + 1);
+					CACHE[row][col] = Math.min(CACHE[row - 1][col], CACHE[row][col - (int) currentCoin] + 1);
 				}
 			}
 		}
 
 		solution = CACHE[CACHE.length - 1][CACHE[0].length - 1];
-		System.out.println("Min number of coins to make " + VALUE + " with coin values " + Arrays.toString(COIN_VALUES) + ": " +solution + "\n");
+		System.out.println("Min number of coins to make " + VALUE + " with coin values " + Arrays.toString(COIN_VALUES)
+				+ ": " + solution + "\n");
 		printCacheToConsole();
 		System.out.println("\n\n");
 		return solution;
@@ -125,7 +134,7 @@ public class CoinChange {
 	}
 
 	// creates a string of hyphens ('-') based off the VALUE's size
-	private static String printBorder() { 
+	private static String printBorder() {
 		StringBuilder string = new StringBuilder();
 
 		string.append("   +");
